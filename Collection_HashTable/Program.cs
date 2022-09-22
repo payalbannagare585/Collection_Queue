59d6c69c4cadb92cd55cae3b4d514ebf6ccedb5f
Gaurav <Gaurav@LAPTOP-TREG306B>
2022-09-22 20:53:48 +0530

    Add project files.

---
+++ Collection_HashTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example of Queue In Collection
+
+            
+            
+                Queue qu = new Queue();
+
+                qu.Enqueue(100);
+                qu.Enqueue(200);
+                qu.Enqueue(300);
+                qu.Enqueue(500);
+
+                qu.Dequeue();
+
+                foreach (var item in qu)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            
+        }
+
+    }
+}
+    
+
---
+++ Collection_Queue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Queue
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        
+        {
+              Hashtable hs = new Hashtable();
+
+              hs.Add(91, "IND");
+              hs.Add(92, "PAK");
+              hs.Add(33, " FR");
+              hs.Add(35, "AUS");
+
+             hs.Remove(33);
+
+         foreach (DictionaryEntry d in hs)
+        {
+         Console.WriteLine($"{d.Key}  {d.Value}");
+         }
+      }
+
+        
+
+}
+        
+
+}
+
+
+
+       
+
+    
+
